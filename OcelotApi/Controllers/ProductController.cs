f233ccec5f35e1bc4382b85f6929cab822da18c8
chi8708 <983143462@qq.com>
2022-07-13 17:25:04 +0800

    添加项目文件

---
+++ OcelotApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OcelotProductApi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("products")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "产品1", "产品2" };
+        }
+    }
+}
