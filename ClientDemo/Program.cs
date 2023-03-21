e6db9b452601efb06787946944d29e1ce4f7b8af
chi8708 <983143462@qq.com>
2023-03-21 16:54:25 +0800

    grpc 初始化测试

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientDemo
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
